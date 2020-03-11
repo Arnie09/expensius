2a62d261c83202e4cceb3d8fc2b9e062c644753c
Arnie09 <arnabchanda964@gmail.com>
2020-03-12 01:10:35 +0530

    added advanced transaction adding options

--- expensius/engine/views.py
+++ expensius/engine/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Transaction
 from login.models import Account
-
+from django.http import JsonResponse
+from datetime import date
+from dateutil.parser import parse
 
 # Create your views here.
 @login_required
@@ -44,27 +46,62 @@ def add_transacton(request):
     # ajax function 
     if request.method =="POST":
 
+        Message = ""
         user = request.user
         direction = request.POST.get('payload[direction]')
         amount = request.POST.get('payload[amount]')
-
+        given_date = parse(request.POST.get('payload[date]')).date()
+        current_date = date.today()
         mapper = {'false':-1,'true':1}
+        change = mapper[direction]*float(amount)
+
         account_obj = Account.objects.get(username = user)
-        account_obj.available_bal += mapper[direction]*float(amount)
-        if direction == 'true':
-            direction = True
-        elif direction == 'false':
-            direction = False
 
-        Transaction.objects.create(
-            account = user,
-            other = request.POST.get('payload[other]'),
-            direction = direction,
-            amount = amount,
-            date = request.POST.get('payload[date]'),
-            amount_accnt = account_obj.available_bal
-            )
+        current_bal = account_obj.available_bal
+        future_bal = current_bal+change
 
-        account_obj.save()
+        if future_bal<0:
+            #error, balance in account cannot be negetive
+            Message = 1
+        elif given_date > current_date:
+            #error, date cannot be a future date
+            Message = 2
+        else:
+            transaction_set = Transaction.objects.filter(date__gt = given_date,date__lte = current_date).order_by('date')
+            flag = 0
+            for transaction in transaction_set:
+                amt = transaction.amount_accnt
+                new_amt = amt+change
+                if new_amt<0:
+                    Message = 3
+                    flag = 1
+                    break
 
-        return redirect('/')
+            if flag == 0:
+    
+                account_obj.available_bal += change
+                if direction == 'true':
+                    direction = True
+                elif direction == 'false':
+                    direction = False
+
+                Transaction.objects.create(
+                    account = user,
+                    other = request.POST.get('payload[other]'),
+                    direction = direction,
+                    amount = amount,
+                    date = request.POST.get('payload[date]'),
+                    amount_accnt = account_obj.available_bal
+                    )
+
+                account_obj.save()
+                Message = 4
+
+                for transaction in transaction_set:
+                    transaction.amount_accnt += change
+                    transaction.save()
+
+        response = {
+            'mssg':Message
+        }
+        return JsonResponse(response)
